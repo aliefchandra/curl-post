e23bb538671e65a6a7e188f7d3d768053d084981
aliefchandra <aliefchandra@gmail.com>
2015-06-20 21:07:07 +0700

    versi 1.0.0

---
+++ function.php
@@ -0,0 +1,66 @@
+<?php
+
+//Upload a blank cookie.txt to the same directory as this file with a CHMOD/Permission to 777
+function login($url,$data){
+    $fp = fopen("cookie.txt", "w");
+    fclose($fp);
+    $login = curl_init();
+    curl_setopt($login, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($login, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($login, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($login, CURLOPT_COOKIEJAR, "cookie.txt");
+    curl_setopt($login, CURLOPT_COOKIEFILE, "cookie.txt");
+    curl_setopt($login, CURLOPT_TIMEOUT, 40000);
+    curl_setopt($login, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($login, CURLOPT_URL, $url);
+    curl_setopt($login, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($login, CURLOPT_POST, TRUE);
+    curl_setopt($login, CURLOPT_POSTFIELDS, $data);
+    ob_start();
+    return curl_exec ($login);
+    ob_end_clean();
+    curl_close ($login);
+    unset($login);    
+}                  
+
+function grab_page($site){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 40);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+    curl_setopt($ch, CURLOPT_URL, $site);
+    ob_start();
+    return curl_exec ($ch);
+    ob_end_clean();
+    curl_close ($ch);
+}
+
+function post_data($site,$data){
+    $datapost = curl_init();
+	$headers = array("Expect:");
+    curl_setopt($datapost, CURLOPT_URL, $site);
+	curl_setopt($datapost, CURLOPT_TIMEOUT, 40000);
+    curl_setopt($datapost, CURLOPT_HEADER, TRUE);
+	curl_setopt($datapost, CURLOPT_HTTPHEADER, $headers); 
+    curl_setopt($datapost, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($datapost, CURLOPT_POST, TRUE);
+    curl_setopt($datapost, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($datapost, CURLOPT_COOKIEFILE, "cookie.txt");
+    ob_start();
+    return curl_exec ($datapost);
+    ob_end_clean();
+    curl_close ($datapost);
+    unset($datapost);    
+}
+function cek_error($result){
+    if ( $result === false){
+        echo 'cURL Error: ' . curl_error($result_curl); //echoes last session error
+    }
+}
+
+
+?>
